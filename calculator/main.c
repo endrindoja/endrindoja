c3a5838aec931e130ecb323e5993aa6b594bcd7f
endrindoja <112771325+endrindoja@users.noreply.github.com>
2022-10-06 19:55:32 +0200

    Add files via upload

---
+++ calculator/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    int y;
+    int v;
+    printf("Put your number");
+    scanf("%d", &x);
+    printf("Put your number");
+    scanf("%d" &y);
+
+    if
+}
